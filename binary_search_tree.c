5a09303540bd604c9e3266f884e1eb16c4e0f376
jabsolution <jay.desai22@gmail.com>
2014-01-26 17:47:26 -0800

    binary_search_tree.c

---
+++ binary_search_tree.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _tree {
+    int ele;
+    struct _tree *left;
+    struct _tree *right;
+} tree;
+
+void insert_tree(tree **, int);
+void print_tree_inorder(tree *);
+
+int main() {
+    int items[]={45, 23, 98, 38, 2, 12, 98, 101, 36};
+    int idx = 0;
+    tree *head = NULL;
+
+    for(idx = 0; idx < 8; idx++) {
+        insert_tree(&head, items[idx]);
+    }
+
+    print_tree_inorder(head);
+}
+
+void print_tree_inorder(tree *head) {
+    if (!head)
+        return;
+    print_tree_inorder(head->left);
+    printf("%d ", head->ele);
+    print_tree_inorder(head->right);
+}
+
+void insert_tree(tree **head, int ele) {
+    tree *node = *head;
+    tree *prevNode;
+
+    while (node) {
+        prevNode = node;
+        if (ele < node->ele) 
+            node = node->left; 
+        else 
+            node = node->right;
+    }
+
+    if (!*head) {
+        node = malloc(sizeof(tree));
+        node->ele = ele;
+        node->left = NULL;
+        node->right = NULL;
+        *head = node;
+    } else if (ele < prevNode->ele) {
+        node = malloc(sizeof(tree));
+        node->ele = ele;
+        node->left = NULL;
+        node->right = NULL;
+        prevNode->left = node;
+    } else {
+        node = malloc(sizeof(tree));
+        node->ele = ele;
+        node->right = NULL;
+        node->left = NULL;
+        prevNode->right = node;
+    }
+}
---
+++ queue.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void qinit(qinfo **q) {
+    *q = malloc(sizeof(qinfo));
+    (*q)->count = 0;
+    (*q)->head = NULL;
+    (*q)->tail = NULL;
+}
+
+void enqueue(qinfo **q, void *ele) {
+    queue *node;
+    if (!(*q)->head) {
+        node = (queue *)malloc(sizeof(queue));
+        node->items = ele;
+        node->next = NULL;
+        (*q)->head = node;
+        (*q)->tail = node; 
+        (*q)->count++;
+        return;
+    }
+    node = (queue *)malloc(sizeof(queue));
+    node->items = ele;
+    node->next = NULL;
+    (*q)->tail->next = node;
+    (*q)->tail = node;
+    (*q)->count++;
+}
+
+queue *dequeue(qinfo **q) {
+    queue *node;
+    if (!(*q)->head) {
+        return NULL;
+    }
+    node = (*q)->head;
+    (*q)->head = node->next;
+    return node;
+}
