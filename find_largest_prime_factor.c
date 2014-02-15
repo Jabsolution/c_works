2ae6247bf33082606808f09bddeb4c3b09bfef77
jabsolution <jay.desai22@gmail.com>
2014-02-14 18:13:17 -0800

    test

--- find_largest_prime_factor.c
+++ find_largest_prime_factor.c
@@ -5,7 +5,7 @@
 long get_the_largest_prime_factor(long);
 
 int main() {
-    long no =  900719925474;
+    long no = 9007199254;
     long largest_prime_factor = 0;
     largest_prime_factor = get_the_largest_prime_factor(no);
     printf("The largest factor is: %ld", largest_prime_factor);
@@ -17,9 +17,11 @@ long get_the_largest_prime_factor(long no) {
     long no_range = 0;
 
     for (no_range = 2; no_range <= range; no_range++) {
-        if (!(no % no_range)) {
+        while (!(no % no_range)) {
             no = no / no_range;
-            factor = no_range;
+            if (no != 1)
+                factor = no;
+            printf("%ld ", no);
         }
     }
     return factor;
