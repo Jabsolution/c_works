baf7bd29bbd7eca41c21c4d63def99bce13b9132
jabsolution <jay.desai22@gmail.com>
2014-02-23 00:23:45 -0800

    Ets

--- bfs.c
+++ bfs.c
@@ -24,7 +24,6 @@ int main() {
     int items[]={45, 23, 98, 38, 2, 12, 98, 101, 36};
     int idx = 0;
     tree *head = NULL;
-    bfs_node *b_node = NULL;
     qinfo *q = NULL;
     tree *tree_node = NULL;
 
@@ -33,31 +32,25 @@ int main() {
      */
     qinit(&q);
 
-    for(idx = 0; idx < 8; idx++) {
+    for(idx = 0; idx <= 8; idx++) {
         insert_tree(&head, items[idx]);
     }
 
-    inorder_print(head);
+    //inorder_print(head);
 
     // Print tree in BFS stlye
-    b_node = create_bfs_node(head);
-    enqueue(&q, (void *)b_node);
+    enqueue(&q, (void *)head);
 
-    while ((b_node = (bfs_node *)dequeue(&q))) {
-        tree_node = (tree *)process_bfs_node_get_tree_node(b_node);
+    while ((tree_node = (tree *)dequeue(&q))) {
         printf("%d ", tree_node->ele);
         if (tree_node->left) {
-            printf("%d ", tree_node->left->ele);
-            b_node = create_bfs_node(tree_node->left);
-            enqueue(&q, (void *)b_node);
+            //printf("queuing left %d\n", tree_node->left->ele);
+            enqueue(&q, (void *)tree_node->left);
         } 
         if (tree_node->right) {
-            printf("%d ", tree_node->right->ele);
-            b_node = create_bfs_node(tree_node->right);
-            enqueue(&q, (void *)b_node);
+            //printf("queue right %d\n", tree_node->right->ele);
+            enqueue(&q, (void *)tree_node->right);
         }
-        printf("Head is %p\n", q->head);
-        printf("Tail is %p\n", q->tail);
     }
 }
 
@@ -69,37 +62,13 @@ void inorder_print(tree *head) {
     inorder_print(head->right);
 }
 
-void *process_bfs_node_get_tree_node(bfs_node *b_node) {
-    if (!b_node)
-        return NULL;
-    return (b_node->tree_node);
-}
-
-bfs_node * create_bfs_node(tree *tree_node) {
-    bfs_node *node;
-    if (!tree_node) 
-        return NULL;
-    init_bfs_node(&node, (void *)tree_node);
-    return node;
-}
-
-void init_bfs_node(bfs_node **q, void *node) {
-    if (!*q)
-        return;
-
-    *q = (bfs_node *)malloc(sizeof(bfs_node));
-    (*q)->tree_node = node;
-    (*q)->color = 0;
-    (*q)->parent = NULL;
-}
-
 void insert_tree(tree **head, int ele) {
     tree *node = *head;
     tree *prevNode;
 
     while (node) {
         prevNode = node;
-        if (ele < (node->ele)) 
+        if (ele <= (node->ele)) 
             node = node->left; 
         else 
             node = node->right;
@@ -111,7 +80,7 @@ void insert_tree(tree **head, int ele) {
         node->left = NULL;
         node->right = NULL;
         *head = node;
-    } else if (ele < prevNode->ele) {
+    } else if (ele <= prevNode->ele) {
         node = malloc(sizeof(tree));
         node->ele = ele;
         node->left = NULL;
